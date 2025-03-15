34886d2c710932d6375d1e4220ad72533dee4de1
alon <alon.ponch@gmail.com>
2025-03-16 00:24:19 +0200

    Working version

--- smali_generator/app/src/main/java/com/smali_generator/TheAmazingPatch.java
+++ smali_generator/app/src/main/java/com/smali_generator/TheAmazingPatch.java
@@ -4,9 +4,9 @@ import android.util.Log;
 import android.view.View;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-class TheAmazingPatch {
+public class TheAmazingPatch {
     static AtomicBoolean is_loaded = new AtomicBoolean(false);
-    public static void on_load(View view) {
+    public static void on_load() {
         if (is_loaded.getAndSet(true)) {
             return;
         }
--- ultimate_patcher/patcher.py
+++ ultimate_patcher/patcher.py
@@ -1,4 +1,5 @@
 import pathlib
+import re
 import shutil
 import lxml.etree
 from androguard.core.apk import APK
@@ -7,6 +8,7 @@ from ultimate_patcher.apk_utils import *
 from ultimate_patcher.config import ManifestKeys
 
 set_log('CRITICAL')
+INVOKE_LINE = '\n\tinvoke-static {}, Lcom/smali_generator/TheAmazingPatch;->on_load()V\n\t'
 
 
 def prepare_smali() -> None:
@@ -30,12 +32,24 @@ def get_activities_with_entry_points(apk_path: str) -> list:
     return activities
 
 
+def patch_or_add_function(smali_file_path: pathlib.Path, function_name: str) -> None:
+    with open(smali_file_path, 'r') as file:
+        smali_file = file.read()
+    matches = re.findall(fr'\.method public [^\n]*{function_name}[^\n]*\n[^\n]+', smali_file)
+    if len(matches) == 0:
+        pass
+    for match in matches:
+        smali_file = smali_file.replace(match, match + INVOKE_LINE)
+    with open(smali_file_path, 'w') as file:
+        file.write(smali_file)
+
+
 def add_static_call_to_on_load(args, class_name: str, function_name: str) -> None:
     smali_file_path = find_smali_file_by_class_name(pathlib.Path(args.temp_path), class_name)
     if smali_file_path is None:
         print(f'[-] Failed to find smali file for {class_name}')
         return
-    print(smali_file_path)
+    patch_or_add_function(smali_file_path, function_name)
 
 
 def patch_entries(args) -> None:
@@ -45,8 +59,7 @@ def patch_entries(args) -> None:
     for activity in activities_to_patch:
         add_static_call_to_on_load(args, activity.get(
             ManifestKeys.TARGET_ACTIVITY if activity.tag == 'activity-alias' else ManifestKeys.NAME),
-                                   'onCreate' if 'activity' in activity.tag else '<clinit>')
-    exit(0)
+                                   'onCreate' if 'activity' in activity.tag else '<init>')
 
 
 def patch_apk(args) -> None:
