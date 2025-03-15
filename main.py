1440d6428f465cc0d4a549a0ab6569a09f7e6194
alon <alon.ponch@gmail.com>
2025-03-16 00:36:16 +0200

    Lint

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import shutil
 from ultimate_patcher.apk_utils import *
 from ultimate_patcher.patcher import patch_apk
 
+
 def get_args():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('-p', '--apk-path', dest='apk_path', help='APK path', required=True)
     parser.add_argument('-o', '--output', dest='output', help='Output APK path', required=False, default='output.apk')
-    parser.add_argument('-t', '--temp', dest='temp_path', help='Temp path for extracted content', required=False, default='./extracted')
+    parser.add_argument('-t', '--temp', dest='temp_path', help='Temp path for extracted content', required=False,
+                        default='./extracted')
     return parser.parse_args()
 
 
@@ -17,9 +19,10 @@ def check_args(args) -> None:
         print('APK file not found')
         exit(1)
 
+
 def clean_up(args):
     shutil.rmtree(args.temp_path, ignore_errors=True)
-    shutil.rmtree(config.SMALI_GENERATOR_OUTPUT_PATH, ignore_errors=True)
+    os.remove(config.SMALI_GENERATOR_OUTPUT_PATH)
     shutil.rmtree(config.SMALI_GENERATOR_SMALI_PATH, ignore_errors=True)
     shutil.rmtree(config.SMALI_GENERATOR_PATH / 'app' / 'build', ignore_errors=True)
 
@@ -42,6 +45,7 @@ def main():
     finally:
         print('[+] Cleaning up...')
         clean_up(args)
+
 
 if __name__ == '__main__':
     main()
