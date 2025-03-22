0aa8dc3fc907900bea962f3dd464353076810d8f
alon <alon.ponch@gmail.com>
2025-03-22 16:52:19 +0200

    Add an example patch

--- smali_generator/app/src/main/java/com/smali_generator/TheAmazingPatch.java
+++ smali_generator/app/src/main/java/com/smali_generator/TheAmazingPatch.java
@@ -7,7 +7,9 @@ import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -18,15 +20,26 @@ import lab.galaxy.yahfa.HookMain;
 public class TheAmazingPatch {
     static AtomicBoolean is_loaded = new AtomicBoolean(false);
 
-    static boolean on_touch_hook(Activity activity, View view, MotionEvent motionEvent) {
-        Log.e("PATCH", "MainActivity.onTouch called: " + activity.toString() + ", "  + view.toString() +", " + motionEvent.toString());
-        boolean ret = TheAmazingPatch.on_touch_hook_backup(activity, view, motionEvent);
-        Log.e("PATCH", "MainActivity.onTouch returned: " + ret);
+    static int decrypt_protobuf_hook(Object self, Object d4o, Object obj, byte[] bArr, int i, int i2, int i3) {
+        Log.e("PATCH", "decrypt_protobuf called: bArr:" + Arrays.toString(bArr));
+        int ret = TheAmazingPatch.decrypt_protobuf_hook_backup(self, d4o, obj, bArr, i, i2, i3);
+        Log.e("PATCH", "decrypt_protobuf returned: " + obj);
+        try {
+            Field view_once_field = obj.getClass().getDeclaredField("viewOnce_");
+            view_once_field.setAccessible(true);
+            boolean is_view_once = (boolean)view_once_field.get(obj);
+            if (is_view_once) {
+                Log.e("PATCH", "viewOnce_ is true");
+                view_once_field.set(obj, false);
+            }
+        } catch (Exception e) {
+            Log.e("PATCH", "Error: " + e.getMessage());
+        }
         return ret;
     }
 
-    static boolean on_touch_hook_backup(Activity activity, View view, MotionEvent motionEvent) {
-        return true;
+    static int decrypt_protobuf_hook_backup(Object self, Object d4o, Object obj, byte[] bArr, int i, int i2, int i3) {
+        return 0;
     }
 
     public static void on_load() {
@@ -35,10 +48,10 @@ public class TheAmazingPatch {
         }
         Log.e("PATCH", "Patch loaded");
         try {
-            Class<?> main_activity = Class.forName("com.androbaby.original2048.MainActivity");
-            Method on_touch_hook_method = TheAmazingPatch.class.getDeclaredMethod("on_touch_hook", Activity.class, View.class, MotionEvent.class);
-            Method on_touch_backup_method = TheAmazingPatch.class.getDeclaredMethod("on_touch_hook_backup", Activity.class, View.class, MotionEvent.class);
-            HookMain.findAndBackupAndHook(main_activity, "onTouch", "(Landroid/view/View;Landroid/view/MotionEvent;)Z", on_touch_hook_method, on_touch_backup_method);
+            Class<?> main_activity = Class.forName("X.Dq4");
+            Method decrypt_protobuf_hook_method = TheAmazingPatch.class.getDeclaredMethod("decrypt_protobuf_hook", Object.class, Object.class, Object.class, byte[].class, int.class, int.class, int.class);
+            Method decrypt_protobuf_hook_method_backup = TheAmazingPatch.class.getDeclaredMethod("decrypt_protobuf_hook_backup", Object.class, Object.class, Object.class, byte[].class, int.class, int.class, int.class);
+            HookMain.findAndBackupAndHook(main_activity, "A0f", "(LX/D4O;Ljava/lang/Object;[BIII)I", decrypt_protobuf_hook_method, decrypt_protobuf_hook_method_backup);
         } catch (Exception e) {
             Log.e("PATCH", "Error: " + e.getMessage());
         }
